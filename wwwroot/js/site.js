a13e7e7938f3253756b758ca124f40d814a45fd2
JavaUroz <urozjavier@gmail.com>
2023-11-01 07:20:21 -0300

    DataTables

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(document).ready(function () {
+    $('#tablas').DataTable({
+        "language": {
+            "url": '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' // Opcional: Traducción al español
+        },
+        "searching": "true",
+
+    });
+});
